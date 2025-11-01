c26b94721ec4418ef238cb7479b18308d4d31367
TheZetmix <proenicso2@gmail.com>
2025-11-01 14:39:58 +0000

    Add new sections
    Also, add include dirs, no-execute, no-autogen sections

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <fstream>
 #include <vector>
 #include "./utils.cpp"
+#include "./thirdparty/ccflags.h"
 using namespace std;
 namespace fs = filesystem;
 
@@ -10,12 +11,15 @@ class FileConfig {
 public:
     string           name;
     vector<fs::path> dependencies;
+    vector<fs::path> include_dirs;
     vector<string>   linked;
     fs::path         output;
     string           compiler = "g++"; // g++ by default
     vector<string>   compiler_options;
     vector<string>   defines;
     vector<string>   cmds;
+    bool             execute = true;   // by default, we execute config
+    bool             autogen = true;
 };
 
 class bsconfigParser {
@@ -48,7 +52,7 @@ public:
             // skip empty lines
             if (line.empty()) continue;
             
-            // update current config name
+            // start new file config section
             if (is_section(line) && line.rfind("[config", 0) == 0) {
                 this->file_configs.push_back(current_config);
                 current_config = {};
@@ -58,6 +62,13 @@ public:
             // if default section
             if (is_section(line)) {
                 current_section = line;
+                
+                // single line sections
+                if (current_section == "[no-execute]") {
+                    current_config.execute = false;
+                } else if (current_section == "[no-autogen]") {
+                    current_config.autogen = false;
+                }
                 continue;
             }
             
@@ -77,6 +88,8 @@ public:
                 current_config.defines.push_back(line);
             } else if (current_section == "[cmds]") {
                 current_config.cmds.push_back(line);
+            } else if (current_section == "[include]") {
+                current_config.include_dirs.push_back(line);
             }
         }
         
@@ -88,7 +101,54 @@ public:
     }
 };
 
+void generate_and_execute(FileConfig cfg) {
+    string generated_cmd = "";
+    
+    if (cfg.autogen) {
+        generated_cmd += cfg.compiler;
+        
+        generated_cmd += " -o ";
+        generated_cmd += cfg.output;
+    }
+    
+    for (auto dep : cfg.dependencies) {
+        generated_cmd += " " + dep.string();
+    }
+    
+    for (auto link : cfg.linked) {
+        generated_cmd += " -l" + link;
+    }
+    
+    for (auto flag : cfg.compiler_options) {
+        generated_cmd += " " + flag;
+    }
+    
+    for (auto define : cfg.defines) {
+        generated_cmd += " " + define;
+    }
+    
+    for (auto include : cfg.include_dirs) {
+        generated_cmd += " -I" + include.string();
+    }
+    
+    if (!generated_cmd.empty()) {
+        zbs::log(generated_cmd);
+        system(generated_cmd.c_str());
+    }
+    
+    for (auto cmd : cfg.cmds) {
+        if (!cmd.empty()) {
+            zbs::log(cmd);
+            system(cmd.c_str());
+        }
+    }
+}
+
 int main(int argc, char** argv) {
+    // setup flags
+    cc_setargs(argc, argv);
+    cc_set_minimum_flags(0);
+    
     // check if .bsconfig exists
     if (!fs::exists("./.zbsconfig")) {
         zbs::error("\".zbsconfig\" not found in current directory");
@@ -97,39 +157,24 @@ int main(int argc, char** argv) {
     bsconfigParser parser;
     parser.parse_file();
     
+    // parse flags
+    if (cc_argexp("-c", "--config")) { // execute single file config by name
+        for (auto i : parser.file_configs) {
+            if (i.name == string(cc_getargexp("-c", "--config"))) {
+                generate_and_execute(i);
+                goto quit;
+            }
+        }
+        zbs::error(string("config " + string(cc_getargexp("-c", "--config")) + " not found"));
+    }
+    
     // generate build commands and execute them
     for (auto i : parser.file_configs) {
-        string generated_cmd = "";
-        
-        generated_cmd += i.compiler;
-        
-        generated_cmd += " -o ";
-        generated_cmd += i.output;
-        
-        for (auto dep : i.dependencies) {
-            generated_cmd += " " + dep.string();
-        }
-        
-        for (auto link : i.linked) {
-            generated_cmd += " -l" + link;
-        }
-        
-        for (auto flag : i.compiler_options) {
-            generated_cmd += " " + flag;
-        }
-        
-        for (auto define : i.defines) {
-            generated_cmd += " " + define;
-        }
-        
-        zbs::log(generated_cmd);
-        system(generated_cmd.c_str());
-        
-        for (auto cmd : i.cmds) {
-            zbs::log(cmd);
-            system(cmd.c_str());
-        }
+        if (i.execute)
+            generate_and_execute(i);
     }
+    
+ quit:
     
     return 0;
 }
